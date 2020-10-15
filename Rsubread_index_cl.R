f074293123ccb7d433dad4459bb1f73567f57809
plague1981 <42053196+plague1981@users.noreply.github.com>
2020-10-15 14:02:35 -0400

    Add files via upload

---
+++ Rsubread_index_cl.R
@@ -0,0 +1,61 @@
+
+
+# ======== Packages required =========
+if("Xmisc" %in% rownames(installed.packages()) == FALSE) {
+  install.packages(Xmisc)}
+library(Xmisc)
+
+# === setting environment ===
+parser <- ArgumentParser$new()
+parser$add_usage('Rsubread_index_cl.R [options]')
+parser$add_description('An executable R script parsing arguments from Unix-like command line.')
+parser$add_argument('--h',type='logical', action='store_true', help='Print the help page')
+parser$add_argument('--help',type='logical',action='store_true',help='Print the help page')
+parser$add_argument('--dir', type = 'character', default = getwd(), help = 'Enter your working directory')
+parser$add_argument('--i', type = 'character', default = 'hg', help = 'Name your index')
+parser$add_argument('--db', type = 'character', default = 'genome.fa', help = 'basename of created index files')
+parser$add_argument('--gI', type = 'logical', default = FALSE, help = 'logical  indicating  if  a  gapped  index  or  a  full  index  will  be  built.A  gappedindex contains 16mers (subreads) that are extracted every three bases from areference genome, whereas a full index contains subreads extracted from everychromosomal location of a genome.   The index contains a hash table,  whichincludes sequences of subreads and their corresponding chromosomal locations.Default value of this argument isFALSE(ie. a full index is built).')
+parser$add_argument('--iS', type = 'logical', default = FALSE, help = 'logical indicating if an index can be split into multiple blocks. The block size isdetermined by value of parametermemory.FALSEby default (ie. a single-blockindex is generated).')
+parser$add_argument('--m', type = 'numeric', default = 8000, help = 'a numeric value specifying the amount of memory (in megabytes) used for storing the index during read mapping. 8000 MB by default. Note that this optionis ignored when indexSplit is FALSE')
+parser$add_argument('--TH', type = 'numeric', default = 100, help = 'a numeric value specifying the threshold for removing highly repetitive subreads(16bp mers).  100 by default.  Subreads will be excluded from the index if theyoccur more than threshold number of times in the genome.')
+parser$add_argument('--cs', type = 'logical', default = FALSE, help = 'logical specifying the mode of the index.  IfTRUE, a color space index will bebuilt. Otherwise, a base space index will be built.')
+parser$helpme()
+# === variables ====
+dirPath <- dir
+dirPath <-gsub ('\\\\','/',dirPath)
+  if (dir.exists(dirPath)){
+    print(paste0("Setting ",dirPath," as the working directory"))
+  } else {
+    print("Directory is not existing!")
+    quit()
+  }
+basename <- i
+database <- db
+
+# ======== Packages required (cont.) =========
+if (!check.packages(Rsubread)){
+  if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+  
+  BiocManager::install("Rsubread")}  
+if (!check.packages(R.utils)){
+  install.packages(R.utils)}
+if (!check.packages(rapportools)){
+  install.packages(rapportools)}
+library(Rsubread)
+library(R.utils)
+library(rapportools)
+
+# ======== build index (biuld index in server if possible) ==========
+if (!file.exists(paste0(basename,".00.b.array"))){
+    if (file.exists(database)){
+      ref<-file.path(dirPath, database)
+      print(paste0("Setting ",database," as the index file for Rsubread"))
+    }else {
+      print(paste0(database," does not exist, please check the filename and try again!"))
+      quit()
+    }
+buildindex(basename = basename, reference = ref, gappedIndex = gI, indexSplit = iS, memory = m, TH_subread = TH, colorspace = cs)
+} else {
+  print(paste(basename,"index exists. Please run Rsubread_align.R"))
+}

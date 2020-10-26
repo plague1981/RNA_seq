6b8ab51e81fe55dfaaf46945a25ac565ba8d3f91
plague1981 <42053196+plague1981@users.noreply.github.com>
2020-10-26 14:59:33 -0400

    Update gff2gtf_cl.R

--- gff2gtf_cl.R
+++ gff2gtf_cl.R
@@ -2,14 +2,15 @@
 if("Xmisc" %in% rownames(installed.packages()) == FALSE) {
   install.packages(Xmisc)}
 # === setting environment ===
-parser <- Xmisc::ArgumentParser$new()
-Xmisc::parser$add_usage('Rsubread_index_cl.R [options]')
-Xmisc::parser$add_description('An executable R script parsing arguments from Unix-like command line.')
-Xmisc::parser$add_argument('--h',type='logical', action='store_true', help='Print the help page')
-Xmisc::parser$add_argument('--help',type='logical',action='store_true',help='Print the help page')
-Xmisc::parser$add_argument('--dir', type = 'character', default = getwd(), help = '"directory",Enter your working directory')
-Xmisc::parser$add_argument('--f', type = 'character', help = '"filename",Enter your *.gff filename')
 
+parser <- Xmisc::ArgumentParser$new()
+parser$add_usage('gff2gtf.R [options]')
+parser$add_description('An executable R script parsing arguments from Unix-like command line. It converts a gff file to gtf file')
+parser$add_argument('--h',type='logical', action='store_true', help='Print the help page')
+parser$add_argument('--help',type='logical',action='store_true',help='Print the help page')
+parser$add_argument('--dir', type = 'character', default = getwd(), help = '"directory",Enter your working directory')
+parser$add_argument('--f', type = 'character', help = '"filename",Enter your *.gff filename')
+parser$helpme()
 # === variables ====
 dirPath <- dir
 dirPath <-gsub ('\\\\','/',dirPath)

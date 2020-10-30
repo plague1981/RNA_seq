96504ce477ba5496822ef504e546d5fffbd58bd3
plague1981 <42053196+plague1981@users.noreply.github.com>
2020-10-30 10:24:32 -0400

    Update merge_fastqgz.R

--- merge_fastqgz.R
+++ merge_fastqgz.R
@@ -36,6 +36,7 @@ library('gsubfn')
 R1.gz.files<-list.files(dirPath, 'L00[1-9]_R1_001.fastq.gz$')
 R2.gz.files<-list.files(dirPath, 'L00[1-9]_R2_001.fastq.gz$')
 # merge R1 files
+require(ShortRead)
 R1.sample_names<-NULL
 for(gz.file in R1.gz.files){
   R1.sample_name<-regmatches(gz.file, gregexpr(".*?_L", gz.file))[[1]]
